a5ac1635365a5c7b73e202207a0b7ba14b8c2cd6
AronXu <aron.xu@hitales.com.cn>
2018-02-28 11:17:52 +0800

    remove old

--- src/main/java/com/example/demo/com/example/demo/config/SDSMongoConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.com.example.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@Configuration
-public class SDSMongoConfig {
-    public static final String MONGO_TEMPLATE = "sdsMongoTemplate";
-}
