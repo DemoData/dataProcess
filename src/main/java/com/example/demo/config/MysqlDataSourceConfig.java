a5a30d0a5fe7882c55a0dd49eb4ab5e9f87fc026
AronXu <aron.xu@hitales.com.cn>
2018-03-07 17:51:45 +0800

    update

--- src/main/java/com/example/demo/config/MysqlDataSourceConfig.java
+++ src/main/java/com/example/demo/config/MysqlDataSourceConfig.java
@@ -16,11 +16,17 @@ import javax.sql.DataSource;
 public class MysqlDataSourceConfig {
 
     public static final String MYSQL_YXZW_PREFIX = "mysql.yixianzhanwei";
-    public static final String MYSQL_JKCT_PREFIX = "mysql.jiankangchati";
     public static final String MYSQL_YXZW_DATASOURCE = "yxzwDataSource";
-    public static final String MYSQL_JKCT_DATASOURCE = "jkctDataSource";
     public static final String MYSQL_YXZW_TEMPLATE = "yxzwJdbcTemplate";
+    public static final String MYSQL_JKCT_PREFIX = "mysql.jiankangchati";
+    public static final String MYSQL_JKCT_DATASOURCE = "jkctDataSource";
     public static final String MYSQL_JKCT_TEMPLATE = "jkctJdbcTemplate";
+    public static final String MYSQL_TNB_PREFIX = "mysql.tangniaobingxiangguan";
+    public static final String MYSQL_TNB_DATASOURCE = "tnbDataSource";
+    public static final String MYSQL_TNB_TEMPLATE = "tnbJdbcTemplate";
+    public static final String MYSQL_YX_PREFIX = "mysql.yixianxiangguan";
+    public static final String MYSQL_YX_DATASOURCE = "yxDataSource";
+    public static final String MYSQL_YX_TEMPLATE = "yxJdbcTemplate";
 
     @Primary
     @Bean(name = MYSQL_YXZW_DATASOURCE)
@@ -37,6 +43,20 @@ public class MysqlDataSourceConfig {
         return DataSourceBuilder.create().build();
     }
 
+    @Bean(name = MYSQL_TNB_DATASOURCE)
+    @Qualifier(MYSQL_TNB_DATASOURCE)
+    @ConfigurationProperties(prefix = MYSQL_TNB_PREFIX)
+    public DataSource tnbDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = MYSQL_YX_DATASOURCE)
+    @Qualifier(MYSQL_YX_DATASOURCE)
+    @ConfigurationProperties(prefix = MYSQL_YX_PREFIX)
+    public DataSource yxDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
     @Bean(name = MYSQL_YXZW_TEMPLATE)
     public JdbcTemplate yxzwJdbcTemplate(@Qualifier(MYSQL_YXZW_DATASOURCE) DataSource dataSource) {
         log.info(dataSource.toString());
@@ -45,6 +65,18 @@ public class MysqlDataSourceConfig {
 
     @Bean(name = MYSQL_JKCT_TEMPLATE)
     public JdbcTemplate jkctJdbcTemplate(@Qualifier(MYSQL_JKCT_DATASOURCE) DataSource dataSource) {
+        log.info(dataSource.toString());
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = MYSQL_TNB_TEMPLATE)
+    public JdbcTemplate tnbJdbcTemplate(@Qualifier(MYSQL_TNB_DATASOURCE) DataSource dataSource) {
+        log.info(dataSource.toString());
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = MYSQL_YX_TEMPLATE)
+    public JdbcTemplate yxJdbcTemplate(@Qualifier(MYSQL_YX_DATASOURCE) DataSource dataSource) {
         log.info(dataSource.toString());
         return new JdbcTemplate(dataSource);
     }
